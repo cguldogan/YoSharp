dea5598260c601c531ce13c2f0250343a46da144
cguldogan <cguldogan@gmail.com>
2014-07-11 01:15:51 -0700

    send individual and count total subsc. added

--- yo/Form1.Designer.cs
+++ yo/Form1.Designer.cs
@@ -31,15 +31,21 @@
             this.btnSendYo = new System.Windows.Forms.Button();
             this.txtToken = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtYoUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblErr = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnSendYo
             // 
-            this.btnSendYo.Location = new System.Drawing.Point(373, 10);
+            this.btnSendYo.Location = new System.Drawing.Point(361, 14);
             this.btnSendYo.Name = "btnSendYo";
-            this.btnSendYo.Size = new System.Drawing.Size(75, 23);
+            this.btnSendYo.Size = new System.Drawing.Size(178, 23);
             this.btnSendYo.TabIndex = 0;
-            this.btnSendYo.Text = "Send YO";
+            this.btnSendYo.Text = "Send A Yo To All Subscribers";
             this.btnSendYo.UseVisualStyleBackColor = true;
             this.btnSendYo.Click += new System.EventHandler(this.btnSendYo_Click);
             // 
@@ -59,13 +65,72 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "your_api_token";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(361, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Yo Individual Usernames";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnYoIndividual_Click);
+            // 
+            // txtYoUserName
+            // 
+            this.txtYoUserName.Location = new System.Drawing.Point(105, 55);
+            this.txtYoUserName.Name = "txtYoUserName";
+            this.txtYoUserName.Size = new System.Drawing.Size(250, 20);
+            this.txtYoUserName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "User Name";
+            // 
+            // lblErr
+            // 
+            this.lblErr.AutoSize = true;
+            this.lblErr.Location = new System.Drawing.Point(367, 93);
+            this.lblErr.Name = "lblErr";
+            this.lblErr.Size = new System.Drawing.Size(0, 13);
+            this.lblErr.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(685, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Count Total Subscribers";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.CountTotal_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Location = new System.Drawing.Point(656, 24);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(13, 13);
+            this.lblCount.TabIndex = 2;
+            this.lblCount.Text = "0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(448, 49);
+            this.ClientSize = new System.Drawing.Size(891, 128);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtYoUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.lblErr);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnSendYo);
             this.Name = "Form1";
             this.Text = "Send YO !!!";
@@ -79,6 +144,12 @@
         private System.Windows.Forms.Button btnSendYo;
         private System.Windows.Forms.TextBox txtToken;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtYoUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblErr;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblCount;
     }
 }
 
--- yo/Form1.cs
+++ yo/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -21,6 +22,10 @@ namespace yo
 
         private void btnSendYo_Click(object sender, EventArgs e)
         {
+
+            if (string.IsNullOrEmpty(txtToken.Text.Trim()))
+                return;
+
             using (var wb = new WebClient())
             {
 
@@ -32,6 +37,66 @@ namespace yo
 
 
                 var response = wb.UploadValues(url, "POST", data);
+
+                //todo: json parse need in here
+                lblErr.Text = string.Format("\nResponse received was :\n{0}", Encoding.ASCII.GetString(response));
+            }
+        }
+
+
+        //not tested yet
+        private void btnYoIndividual_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtYoUserName.Text.Trim()) & string.IsNullOrEmpty(txtToken.Text.Trim()))
+                return;
+
+            using (var wb = new WebClient())
+            {
+                var data = new NameValueCollection();
+
+                string token = txtToken.Text;
+                string url = "http://api.justyo.co/yo/";
+                data["api_token"] = token;
+                data["username"] = txtYoUserName.Text.Trim();
+
+                var response = wb.UploadValues(url, "POST", data);
+
+                //todo: json parse need in here
+                lblErr.Text = string.Format("\nResponse received was :\n{0}", Encoding.ASCII.GetString(response));
+            }
+
+        }
+
+        private void CountTotal_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtToken.Text.Trim()))
+                return;
+
+            using (var wb = new WebClient())
+            {
+
+                var token = txtToken.Text;
+
+                var uri = @"http://api.justyo.co/subscribers_count/?api_token="+ token;
+
+                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(uri);
+                request.Method = "GET";
+                String test = String.Empty;
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    Stream dataStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(dataStream);
+                    test = reader.ReadToEnd();
+
+                    //todo: json parse need in here
+                    test = test.Split(':')[1].Replace("}", "");
+
+                    reader.Close();
+                    dataStream.Close();
+
+                    lblCount.Text = test;
+                }
+
             }
         }
     }
