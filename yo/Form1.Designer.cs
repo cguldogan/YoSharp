accaad8dea443f3ed3135a59a8f164be3462ec54
cguldogan <cguldogan@gmail.com>
2014-07-10 23:46:57 -0700

    init

---
+++ yo/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace yo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSendYo = new System.Windows.Forms.Button();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSendYo
+            // 
+            this.btnSendYo.Location = new System.Drawing.Point(373, 10);
+            this.btnSendYo.Name = "btnSendYo";
+            this.btnSendYo.Size = new System.Drawing.Size(75, 23);
+            this.btnSendYo.TabIndex = 0;
+            this.btnSendYo.Text = "Send YO";
+            this.btnSendYo.UseVisualStyleBackColor = true;
+            this.btnSendYo.Click += new System.EventHandler(this.btnSendYo_Click);
+            // 
+            // txtToken
+            // 
+            this.txtToken.Location = new System.Drawing.Point(105, 13);
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(250, 20);
+            this.txtToken.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "your_api_token";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(448, 49);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.btnSendYo);
+            this.Name = "Form1";
+            this.Text = "Send YO !!!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSendYo;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ yo/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace yo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSendYo_Click(object sender, EventArgs e)
+        {
+            using (var wb = new WebClient())
+            {
+                var data = new NameValueCollection();
+
+                string token = txtToken.Text;
+                string url = "http://api.justyo.co/yoall/";
+                data["api_token"] = token;
+
+
+                var response = wb.UploadValues(url, "POST", data);
+            }
+        }
+    }
+}
